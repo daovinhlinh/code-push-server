af17a067935945d0f2ba64221a7211513f499ba0
Dao Vinh Linh <linhdv@nhsv.vn>
2025-02-20 10:05:16 +0700

    Add azurite

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ cd api
 npm install # Required because npm ci will install only prod dependencies because of app services environment
 npm run clean
 npm run build
-
+npm install -g azurite
 # rm -rf /home/site/wwwroot/*
 # cp -r /home/site/repository/api/* /home/site/wwwroot/
